2b17782ebbc977bf944cfe64fc10b7a04c502f2b
b-price <bprice12@students.towson.edu>
2025-07-01 13:08:24 -0400

    fallback to artist name on last.fm mbid query fail

--- controllers/lastfmArtistData.ts
+++ controllers/lastfmArtistData.ts
@@ -29,7 +29,11 @@ export const getArtistData = async (mbid: string, artistName: string) => {
             mbid: data.mbid,
             ontour: parseInt(data.ontour) === 1,
             stats: {listeners: parseInt(data.stats.listeners), playcount: parseInt(data.stats.playcount)},
-            bio: {link: data.bio.links.link.href, summary: data.bio.summary, content: data.bio.content.split('<a')[0]},
+            bio: {
+                link: data.bio.links.link.href,
+                summary: data.bio.summary.replaceAll(/<.*>/g, ''),
+                content: data.bio.content.replaceAll(/<.*>/g, '')
+            },
             similar: data.similar.artist.map((a: {name: string}) => a.name),
             date: new Date().toISOString()
         }
@@ -54,7 +58,11 @@ export const getArtistData = async (mbid: string, artistName: string) => {
                 mbid: data.mbid,
                 ontour: parseInt(data.ontour) === 1,
                 stats: {listeners: parseInt(data.stats.listeners), playcount: parseInt(data.stats.playcount)},
-                bio: {link: data.bio.links.link.href, summary: data.bio.summary, content: data.bio.content.split('<a')[0]},
+                bio: {
+                    link: data.bio.links.link.href,
+                    summary: data.bio.summary.replaceAll(/<.*>/g, ''),
+                    content: data.bio.content.replaceAll(/<.*>/g, '')
+                },
                 similar: data.similar.artist.map((a: {name: string}) => a.name),
                 date: new Date().toISOString()
             }
