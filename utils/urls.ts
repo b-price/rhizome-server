2a7e815ac4e37db4d9ddece8e65584c31889fb4b
b-price <bprice12@students.towson.edu>
2025-11-04 10:50:46 -0500

    added frontend url condition for mobile

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,8 +1,11 @@
 export const SERVER_PROD_URL = 'https://api.rhizome.fyi';
 export const SERVER_DEV_URL = 'https://api.dev.rhizome.fyi';
+export const SERVER_MOBILE_DEV_URL = 'https://api.dev.mobile.rhizome.fyi';
 
 export const serverUrl = process.env.SERVER === 'production'
-    ? process.env.CLIENT === 'production' ? SERVER_PROD_URL : SERVER_DEV_URL
+    ? process.env.CLIENT === 'production'
+        ? SERVER_PROD_URL : process.env.CLIENT === 'mobiledev'
+            ? SERVER_MOBILE_DEV_URL  : SERVER_DEV_URL
     : 'http://localhost:3000';
 
 export const ngrokUrl = process.env.SERVER === 'production'
@@ -11,5 +14,7 @@ export const ngrokUrl = process.env.SERVER === 'production'
 
 export const FRONTEND_DEPLOYMENT_URL = 'https://www.rhizome.fyi';
 export const FRONTEND_LOCALHOST = 'http://localhost:5173';
+export const FRONTEND_LOCAL_MOBILE = 'http://192.168.86.242:5173'
 
-export const frontend_url = process.env.CLIENT === 'production' ? FRONTEND_DEPLOYMENT_URL : FRONTEND_LOCALHOST;
+export const frontend_url = process.env.CLIENT === 'production' ? FRONTEND_DEPLOYMENT_URL
+    : process.env.CLIENT === 'mobiledev' ? FRONTEND_LOCAL_MOBILE : FRONTEND_LOCALHOST;
