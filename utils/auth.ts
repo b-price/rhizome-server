9940ede46ed9367a3d1b8b6220792ca531566655
b-price <bprice12@students.towson.edu>
2025-12-02 12:57:56 -0500

    undo email security fix

--- utils/auth.ts
+++ utils/auth.ts
@@ -14,7 +14,7 @@ export const auth = () => betterAuth({
         enabled: true,
         autoSignIn: true,
         sendResetPassword: async ({ user, url, token }, request) => {
-            void sendEmail({
+            await sendEmail({
                 to: user.email,
                 from: ADMIN_EMAIL,
                 subject: 'Reset your Rhizome password',
@@ -56,7 +56,7 @@ export const auth = () => betterAuth({
                 },
                 request  // The original request object (optional)
             ) => {
-                void sendEmail({
+                await sendEmail({
                     to: user.email,
                     from: ADMIN_EMAIL,
                     subject: 'Confirm your Rhizome account deletion',
