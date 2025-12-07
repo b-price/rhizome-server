af505d088c4657ac4c4e7365b4fadab19035d122
b-price <bprice12@students.towson.edu>
2025-12-07 13:44:51 -0500

    email try catch and error logs

--- utils/email.ts
+++ utils/email.ts
@@ -2,15 +2,21 @@ import nodemailer from 'nodemailer';
 import {ADMIN_EMAIL} from "./defaults";
 
 export async function sendEmail(email: { to: string, from: string, subject: string, text?: string, html?: string }) {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: ADMIN_EMAIL,
-            clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
-        }
-    });
-    return await transporter.sendMail(email);
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: ADMIN_EMAIL,
+                clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+                refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN,
+            }
+        });
+        return await transporter.sendMail(email);
+    } catch (err) {
+        console.error(err);
+        return err
+    }
+
 }
